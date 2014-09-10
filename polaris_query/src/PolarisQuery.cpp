24bf281a6155dde74b5ea7ff90ec15135ff37fe1
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-09-10 09:39:19 -0700

    Make this an application and wait for termination request

--- polaris_query/src/PolarisQuery.cpp
+++ polaris_query/src/PolarisQuery.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include "PolarisQuery.h"
 #include "Poco/Net/TCPServer.h"
 #include "QueryServerFactory.h"
 using namespace std;
 using Poco::Net::TCPServer;
 using Poco::Net::ServerSocket;
 
-int main() {
+int main(int argc, char** argv) {
+    PolarisQuery app;
+    return app.run(argc, argv);
+}
+
+int PolarisQuery::main(const std::vector<std::string> &args) {
     ServerSocket mySocket((Poco::UInt16) 1337);
     QueryServerConnectionFactory* theFactory = new QueryServerConnectionFactory;
     TCPServer theServer(theFactory, mySocket);
@@ -13,6 +19,8 @@ int main() {
     cout << "Listening on port 1337 for connections.";
 
     theServer.start();
+
+    waitForTerminationRequest();
 
     delete theFactory;
 
---
+++ polaris_query/src/PolarisQuery.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Poco/Util/ServerApplication.h"
+using Poco::Util::ServerApplication
+
+class PolarisQuery : public ServerApplication {
+public:
+    int main(const std::vector<std::string> & args);
+};
