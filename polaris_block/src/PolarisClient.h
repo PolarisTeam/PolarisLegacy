0a901ab24e1c4b3b5f69956dd1921b3b656998cb
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-09-12 14:11:05 -0700

    More things

--- polaris_block/src/PolarisClient.h
+++ polaris_block/src/PolarisClient.h
@@ -6,4 +6,9 @@ class PolarisClient {
 public:
     PolarisClient(PolarisConnection connection);
     ~PolarisClient();
+
+private:
+    PolarisConnection theConnection;
 };
+
+std::vector<PolarisClient> clients;
