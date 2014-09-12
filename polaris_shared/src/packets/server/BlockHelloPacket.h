c6b607d9b3665acb3f3309e7250cd751e00533f5
Treeki <treeki@gmail.com>
2014-09-13 00:31:44 +0200

    Fix BlockHelloPacket

--- polaris_shared/src/packets/server/BlockHelloPacket.h
+++ polaris_shared/src/packets/server/BlockHelloPacket.h
@@ -3,8 +3,15 @@
 
 class BlockHelloPacket : public Packet {
 public:
-    BlockHelloPacket(uint16 blockCode) : blockCode(blockCode) {};
-    PacketData build() { return PacketData(PacketHeader(16, 3, 8, 0, 0), &blockCode,16); }
+    BlockHelloPacket(uint16_t blockCode) {
+        data[0] = 3;
+        data[1] = blockCode;
+        data[2] = 0;
+        data[3] = 0;
+    }
+    PacketData build() {
+        return PacketData(PacketHeader(16, 3, 8, 0, 0), &data);
+    }
 private:
-    uint16_t blockCode;
+    uint16_t data[4];
 };
