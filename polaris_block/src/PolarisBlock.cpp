c9926e34905a8bd9586fe57763eb834343675237
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-09-12 13:10:40 -0700

    Add PolarisBlock server app

---
+++ polaris_block/src/PolarisBlock.cpp
@@ -0,0 +1,10 @@
+#include "PolarisBlock.h"
+
+int main(int argc, char** argv) {
+    return PolarisBlockApp().run(argc, argv);
+}
+
+int PolarisBlockApp::main(const std::vector<std::string> &args) {
+
+    return 0;
+}
---
+++ polaris_block/src/PolarisBlock.h
@@ -0,0 +1,8 @@
+#import "Poco/Util/ServerApplication.h"
+
+using Poco::Util::ServerApplication
+
+class PolarisBlockApp : public ServerApplication {
+public:
+    int main(const std::vector<std::string> & args);
+};
