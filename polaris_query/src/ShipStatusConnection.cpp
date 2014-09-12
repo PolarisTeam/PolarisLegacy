a5f8852c355a8b25e6e30c59fa931b720b06acef
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-09-12 12:54:13 -0700

    ShipStatus: Uncomment ship 2

--- polaris_query/src/ShipStatusConnection.cpp
+++ polaris_query/src/ShipStatusConnection.cpp
@@ -14,7 +14,7 @@ void ShipStatusConnection::run() {
     ShipListPacket shipList = ShipListPacket();
     shipList.addShip(1000, u"Ship01", 210,189,208,1, SHIP_OFFLINE, 1, 0x8D677);
     shipList.addShip(2000, u"Ship02", 127,0,0,1, SHIP_ONLINE, 2, 0x98E0C);
-    //shipList.addShip(3000, u"Ship03", 210,189,208,31, SHIP_OFFLINE, 3, 0xB13B6);
+    shipList.addShip(3000, u"Ship03", 210,189,208,31, SHIP_OFFLINE, 3, 0xB13B6);
     shipList.addShip(4000, u"Ship04", 210,189,208,46, SHIP_OFFLINE, 4, 0xB1389);
     shipList.addShip(5000, u"Ship05", 210,189,208,61, SHIP_OFFLINE, 5, 0xB13A2);
     shipList.addShip(6000, u"Ship06", 210,189,208,76, SHIP_OFFLINE, 6, 0xADA99);
