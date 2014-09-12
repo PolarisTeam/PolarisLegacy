3ca0d4998acd7d1542129908b9de3d827672afa1
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-09-12 13:44:07 -0700

    Base reactor framework

--- polaris_block/src/PolarisBlock.cpp
+++ polaris_block/src/PolarisBlock.cpp
@@ -1,10 +1,37 @@
 #include "PolarisBlock.h"
 
+#include "Poco/Net/ServerSocket.h"
+#include "Poco/Net/SocketReactor.h"
+#include "Poco/Net/SocketAcceptor.h"
+
+using Poco::Net::ServerSocket;
+using Poco::Net::SocketReactor;
+using Poco::Net::SocketAcceptor;
+using Poco::Thread;
+
 int main(int argc, char** argv) {
     return PolarisBlockApp().run(argc, argv);
 }
 
 int PolarisBlockApp::main(const std::vector<std::string> &args) {
+    // Create + Bind socket
+    ServerSocket blockSocket(12201); // TODO Config file / Argument / Whatever
 
-    return 0;
+    // Make the reactor
+    SocketReactor reactor;
+
+    // Assign the Connector
+    //TODO
+
+    Thread thread;
+    thread.start(reactor);
+
+    waitForTerminationRequest();
+
+    reactor.stop();
+    thread.join();
+
+
+
+    return Application::EXIT_OK;
 }
