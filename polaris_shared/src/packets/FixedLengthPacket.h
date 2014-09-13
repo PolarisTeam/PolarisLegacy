776dccdc564b6c8f142e9853a8f9725384333e19
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-09-13 10:38:54 -0700

    I'm good at things

--- polaris_shared/src/packets/FixedLengthPacket.h
+++ polaris_shared/src/packets/FixedLengthPacket.h
@@ -1,7 +1,7 @@
 #include "Packet.h"
 
 struct MysteryPacket{
-    PacketHeader header = PacketHeader(0x10, 0x11, 0x1, 0x0, 0x0);
+    PacketHeader header = PacketHeader(0x10, 0x11, 0x49, 0x0, 0x0);
     uint16_t randomDataOfDoom;
     MysteryPacket(uint16_t randomData) {
         randomDataOfDoom = randomData;
