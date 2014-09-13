3e6ad835700d6d023166dec8c282e7cbc4275e99
Treeki <treeki@gmail.com>
2014-09-13 04:32:15 +0200

    Implement RC4 key exchange and client activation packet

--- polaris_block/src/PolarisConnection.cpp
+++ polaris_block/src/PolarisConnection.cpp
@@ -1,5 +1,9 @@
 #include <packets/Packet.h>
 #include "packets/server/BlockHelloPacket.h"
+#include "Poco/Crypto/RSAKey.h"
+#include "Poco/Crypto/Cipher.h"
+#include "Poco/Crypto/CipherKey.h"
+#include "Poco/Crypto/CipherFactory.h"
 #include <string.h>
 #include "PolarisConnection.h"
 #include "PolarisClient.h"
@@ -8,7 +12,8 @@ PolarisConnection::PolarisConnection(const StreamSocket& socket, SocketReactor& 
     socket(socket),
     reactor(reactor),
     bufferPtr(new uint8_t[BUFFER_SIZE]),
-    bufferPosition(0)
+    bufferPosition(0),
+    outputTransform(0), inputTransform(0)
 {
     this->client = new PolarisClient(this);
 
@@ -16,8 +21,8 @@ PolarisConnection::PolarisConnection(const StreamSocket& socket, SocketReactor& 
     reactor.addEventHandler(socket, NObserver<PolarisConnection, ShutdownNotification>(*this, &PolarisConnection::onShutdown));
 
     BlockHelloPacket hello(201); // TODO Dynamic block pls
-    PacketData helloData = hello.build(); // TODO Make this cleaner
-    this->socket.sendBytes(helloData.getData(), helloData.getSize());
+    PacketData helloData = hello.build();
+    sendPacket(helloData);
 }
 
 PolarisConnection::~PolarisConnection() {
@@ -26,6 +31,9 @@ PolarisConnection::~PolarisConnection() {
 
     reactor.removeEventHandler(socket, NObserver<PolarisConnection, ReadableNotification>(*this, &PolarisConnection::onReadable));
     reactor.removeEventHandler(socket, NObserver<PolarisConnection, ShutdownNotification>(*this, &PolarisConnection::onShutdown));
+
+    delete outputTransform;
+    delete inputTransform;
 }
 
 void PolarisConnection::onShutdown(AutoPtr<ShutdownNotification> const &notification) {
@@ -40,7 +48,15 @@ void PolarisConnection::onReadable(AutoPtr<ReadableNotification> const &notifica
     */
 
     // Fill up the buffer, as much as possible
-    int readAmount = socket.receiveBytes(&bufferPtr[bufferPosition], BUFFER_SIZE - bufferPosition);
+    int readAmount;
+
+    if (inputTransform) {
+        uint8_t encrypted[BUFFER_SIZE];
+        readAmount = socket.receiveBytes(encrypted, BUFFER_SIZE - bufferPosition);
+        inputTransform->transform(encrypted, readAmount, &bufferPtr[bufferPosition], readAmount);
+    } else {
+        readAmount = socket.receiveBytes(&bufferPtr[bufferPosition], BUFFER_SIZE - bufferPosition);
+    }
     bufferPosition += readAmount;
 
     if (readAmount <= 0) {
@@ -77,9 +93,95 @@ void PolarisConnection::onReadable(AutoPtr<ReadableNotification> const &notifica
 }
 
 
+void PolarisConnection::sendPacket(PacketData &data) {
+    PacketHeader *header = (PacketHeader *)data.getData();
+    printf("[Sending packet : %d bytes, type %x-%x]\n", header->length, header->command, header->subcommand);
+
+    if (outputTransform) {
+        uint8_t *encoded = new uint8_t[data.getSize()];
+        this->outputTransform->transform(data.getData(), data.getSize(), encoded, data.getSize());
+        this->socket.sendBytes(encoded, data.getSize());
+        delete[] encoded;
+    } else {
+        this->socket.sendBytes(data.getData(), data.getSize());
+    }
+}
+
+
 void PolarisConnection::handlePacket(uint8_t *packet) {
     PacketHeader *header = (PacketHeader *)packet;
 
+    // Incomplete (corrupted/malicious?) packet
+    if (header->length < 8)
+        return;
+
     printf("[Received packet : %d bytes, type %x-%x]\n", header->length, header->command, header->subcommand);
+
+    if (header->command == 0x11 && header->subcommand == 0xB) {
+        // Key exchange
+        handleKeyExchange(packet);
+        return;
+    }
+}
+
+
+void PolarisConnection::handleKeyExchange(uint8_t *packet) {
+    PacketHeader *header = (PacketHeader *)packet;
+
+    if (header->length < 0x88)
+        return;
+
+    // Cache this key at some point.. maybe?
+    Cipher *rsa = 0;
+    CryptoTransform *dec = 0;
+
+    try {
+        RSAKey rsaKey("", "privateKey.pem");
+        rsa = CipherFactory::defaultFactory().createCipher(rsaKey);
+        dec = rsa->createDecryptor();
+
+        // Reverse the input (encrypted) data
+        uint8_t input[0x80], output[0x80];
+        for (int i = 0; i < 0x80; i++)
+            input[i] = packet[0x87 - i];
+
+        int processed = dec->transform(input, sizeof(input), output, sizeof(output));
+        processed += dec->finalize(output, sizeof(output) - processed);
+
+        if (processed >= 0x20) {
+            // Valid key exchange
+            // First 16 bytes: Challenge data, encrypted with RC4 key
+            // Following 16 bytes: RC4 key
+
+            std::vector<uint8_t> rc4key(16), rc4iv;
+            memcpy(rc4key.data(), &output[16], 16);
+
+            CipherKey rc4ck("rc4", rc4key, rc4iv);
+            cipher = CipherFactory::defaultFactory().createCipher(rc4ck);
+
+            // First off, generate the response to activate the client
+            CryptoTransform *dec = cipher->createDecryptor();
+
+            uint8_t response[16];
+            dec->transform(output, 16, response, 16);
+
+            delete dec;
+
+            // Second, enable RC4 encryption on this connection
+            outputTransform = cipher->createEncryptor();
+            inputTransform = cipher->createDecryptor();
+
+            // Build the response packet
+            PacketData responsePacket(PacketHeader(0x18, 0x11, 0xC, 0, 0), response);
+            sendPacket(responsePacket);
+        }
+
+    } catch (Poco::Exception &e) {
+
+    }
+
+    if (rsa)
+        rsa->release();
+    delete dec;
 }
 
--- polaris_block/src/PolarisConnection.h
+++ polaris_block/src/PolarisConnection.h
@@ -5,10 +5,14 @@
 #include "Poco/Net/SocketNotification.h"
 #include "Poco/AutoPtr.h"
 #include "Poco/NObserver.h"
+#include "Poco/Crypto/Cipher.h"
+#include "Poco/Crypto/CryptoTransform.h"
 
 class PolarisClient;
+class PacketData;
 
 using namespace Poco::Net;
+using namespace Poco::Crypto;
 using Poco::AutoPtr;
 using Poco::NObserver;
 
@@ -19,15 +23,22 @@ public:
     void onShutdown(const AutoPtr<ShutdownNotification> &);
     ~PolarisConnection();
 
+    void sendPacket(PacketData &data);
+
 private:
     StreamSocket socket;
     SocketReactor& reactor;
     PolarisClient* client;
+
+    Poco::AutoPtr<Cipher> cipher;
+    CryptoTransform *outputTransform, *inputTransform;
 
     enum { BUFFER_SIZE = 1024 * 16 };
     int bufferPosition; // current end position of the data in the buffer
     uint8_t* bufferPtr;
 
     void handlePacket(uint8_t *packet);
+
+    void handleKeyExchange(uint8_t *packet);
 
 };
