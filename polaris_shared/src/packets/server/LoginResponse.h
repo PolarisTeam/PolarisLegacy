3cc2abf0d1e6c5acd90257db1f292f0654b4a5dd
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-10-02 16:51:13 -0700

    Add know fields to the LoginResponse header

--- polaris_shared/src/packets/server/LoginResponse.h
+++ polaris_shared/src/packets/server/LoginResponse.h
@@ -1,9 +1,14 @@
 #include "../Packet.h"
+#include <string>
 
 class LoginResponsePacket : public Packet {
 public:
-    LoginResponsePacket();
+    LoginResponsePacket(std::u16string loginErr, std::u16string blockName); //TODO: Put all fields in the constructor OR use templates or something to make this less stupid
     ~LoginResponsePacket();
 
     PacketData build();
+
+private:
+    std::u16string loginErr;
+    std::u16string blockName;
 };
