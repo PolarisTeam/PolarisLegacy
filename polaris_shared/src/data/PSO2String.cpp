2893ea69c49ba9bd06d45975e9d1edf4f570303b
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-10-02 19:12:24 -0700

    PSO2String fixes

--- polaris_shared/src/data/PSO2String.cpp
+++ polaris_shared/src/data/PSO2String.cpp
@@ -6,13 +6,20 @@ Polaris::Data::PSO2String Polaris::Data::CreatePSO2String(std::u16string sourceS
 
     theString.utf16string = sourceString;
 
-    theString.magicValue = (uint32_t) (((theString.utf16string.size() + 1) + subValue) ^ xorValue);
-
-    if ((theString.utf16string.size() & 1) == 0) {
-        // add a padding character
-        theString.utf16string.push_back(0);
+    if(sourceString.empty()) {
+        theString.magicValue = (uint32_t) ((subValue) ^ xorValue);
+        theString.dataLength = 0;
+    } else {
+        theString.magicValue = (uint32_t) (((theString.utf16string.size() + 1) + subValue) ^ xorValue);
+        if ((theString.utf16string.size() & 1) == 0) {
+            // add a padding character
+            theString.utf16string.push_back(0);
+        }
+        theString.dataLength = (uint32_t) ((theString.utf16string.size() + 1) * 2);
     }
-    theString.dataLength = (uint32_t) ((theString.utf16string.size() + 1) * 2);
+
+
+
 
     return theString;
 }
