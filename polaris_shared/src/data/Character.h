7a9ac23c96dff0a0d1db3477facb731ab353aadd
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-10-12 00:42:53 -0700

    Create some base character structures, add character spawn packet.

--- polaris_shared/src/data/Character.h
+++ polaris_shared/src/data/Character.h
@@ -1,26 +1,79 @@
-#include <bits/stringfwd.h>
-
 struct CharBlob1 {
     // 0x1AC Big
     uint16_t nameBuffer[16];
     uint8_t padding[4] = {0, 0, 0, 0};
-
-
 };
 
 struct CharBlob2 {
-// 0x230 Big
     uint8_t unknown[0x230];
 };
 
-class Character {
-public:
-    Character();
-    ~Character();
+struct CharacterHeaderMaybe {
+    uint32_t playerId;
+    uint32_t unknown_4;
+    uint16_t unknown_8;
+    uint16_t unknown_A;
+};
 
-public: //For now
-    std::u16string name;
+struct MysteryCharacter14 {
+    uint16_t unknown_0;
+    uint16_t unknown_2;
+    uint16_t unknown_4;
+    uint16_t unknown_6;
+    uint16_t unknown_7;
+    uint16_t unknown_A;
+    uint16_t unknown_C;
+};
 
+struct CharacterLooksParameter {
+    char unknown_data[175];
+    char unknown_AF;
+};
 
-
+struct CharacterJobParameter {
+    char unknown_0;
+    char unknown_1;
+    char unknown_2;
+    char unknown_3;
+    uint32_t unknown_4;
+    uint16_t unknown_8;
+    uint16_t unknown_A;
+    uint32_t unknown_C;
+    uint16_t unknown_10;
+    uint16_t unknown_12;
+    uint32_t unknown_14;
+    uint16_t unknown_18;
+    uint16_t unknown_1A;
+    uint32_t unknown_1C;
+    uint16_t unknown_20;
+    uint16_t unknown_22;
+    uint32_t unknown_24;
+    uint16_t unknown_28;
+    uint16_t unknown_2A;
+    uint32_t unknown_2C;
+    uint16_t unknown_30;
+    uint16_t unknown_32;
+    uint32_t unknown_34;
+    uint16_t unknown_38;
+    uint16_t unknown_3A;
+    uint32_t unknown_3C;
+    uint16_t unknown_40;
+    uint16_t unknown_42;
+    uint32_t unknown_44;
+    uint16_t unknown_48;
+    uint16_t unknown_4A;
+    uint32_t unknown_4C;
+    uint16_t unknown_50;
+    uint16_t unknown_52;
+    uint32_t unknown_54;
+    uint16_t unknown_58;
+    uint16_t unknown_5A;
+    uint32_t unknown_5C;
+    uint16_t unknown_60;
+    uint16_t unknown_62;
+    uint32_t unknown_64;
+    uint32_t unknown_68;
+    uint16_t unknown_6C;
+    uint16_t unknown_6E;
+    unsigned __int128 unknown_70;
 };
--- polaris_shared/src/packets/FixedLengthPacket.h
+++ polaris_shared/src/packets/FixedLengthPacket.h
@@ -1,4 +1,5 @@
 #include "Packet.h"
+#include "../data/Character.h"
 
 struct MysteryPacket{
     PacketHeader header = PacketHeader(0xC, 0x11, 0x49, 0x0, 0x0);
@@ -8,6 +9,30 @@ struct MysteryPacket{
     }
 };
 
+struct CharacterSpawnPacket {
+    PacketHeader header;
+    CharacterHeaderMaybe charHeaderMaybe;
+    MysteryCharacter14 unknown_14;
+    char unk_22[34];
+    uint16_t unknown_44;
+    char unk_46[6];
+    uint32_t unknown_4c;
+    char unk_50[8];
+    uint32_t unknown_58;
+    uint32_t unknown_5C;
+    uint32_t playerIdCopy;
+    char unk_64[4];
+    char voiceParam_unknown4;
+    char unk_69[3];
+    uint32_t voiceParam_unknown8;
+    uint16_t name[16]; //TODO: Find a 16-bit consistant wchar_t.
+    char unk_90[4];
+    CharacterLooksParameter looksParam;
+    CharacterJobParameter jobsParam;
+    uint32_t unknown_1C4[16];
+    char unk_204[2];
+    char unknown_206;
+};
 
 class FixedLengthPacket : public Packet
 {
